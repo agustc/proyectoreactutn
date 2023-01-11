2d1c33fadee62e332fddf6c99f8d484826e15dd4
Agustina <agustinaceba@hotmail.com>
2023-01-11 20:37:42 -0300

    navbar responsive

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./components/styles.css"
+import Navbarr from "./components/navbar/Navbar";
+import {Routes} from "react-router-dom";
+
+
+export default function App(){
+  return(
+    <div className="App">
+    <Navbarr/>
+    </div>
+  
+);
+}
+
---
+++ src/components/navbar/MenuItems.js
@@ -0,0 +1,37 @@
+
+
+export const MenuItems = [
+   {
+    title:"Home",
+    url : "/",
+    cName:"nav-links",
+    icon : "fa-solid fa-house-user"
+   },
+
+   {
+    title:"Productos",
+    url : "/Productos",
+    cName:"nav-links",
+    icon : "fa-solid fa-seedling"
+   },
+   
+   {
+    title:"Sucursales",
+    url : "/Sucursales",
+    cName:"nav-links",
+    icon : "fa-solid fa-location-pin"
+   },
+
+   {
+    title:"Nosotros",
+    url : "/Nosotros",
+    cName:"nav-links",
+    icon : "fa-solid fa-people-group"
+   },
+{
+   title:"Contacto",
+   url : "/Contacto",
+   cName:"nav-links",
+   icon :"fa-solid fa-phone"
+  }
+]
